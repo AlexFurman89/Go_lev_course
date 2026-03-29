84cd29f81058a4343d11c8af3e71eda6508ebb07
alexfurman <alexfurman20@gmail.com>
2026-03-29 23:01:49 +0300

    laba_7

---
+++ laba_7/models/note.go
@@ -0,0 +1,14 @@
+package models
+
+//go:generate easyjson -all note.go
+
+type Note struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+var Notes = []Note{
+	{ID: 1, Title: "Buy milk", Content: "2 liters"},
+	{ID: 2, Title: "Read book", Content: "10 pages"},
+}
